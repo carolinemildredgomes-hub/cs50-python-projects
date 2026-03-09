a3c838a01fa215ba7a4bb7be239763c0e651d95c
Caroline Mildred Gomes <carolinemildredgomes@gmail.com>
2026-03-09 19:16:49 +0600

    Add files via upload

--- playback/playback.py
+++ playback/playback.py
@@ -1,1 +1,8 @@
+# Ask the user for input
+sentence = input("Type a sentence: ")
 
+# Replace spaces with "..."
+slower_sentence = sentence.replace(" ", "...")
+
+# Print the result
+print(slower_sentence)
