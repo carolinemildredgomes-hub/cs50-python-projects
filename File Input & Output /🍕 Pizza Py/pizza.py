6b7a3c22fbd782fdcc64dbcabee445c433559866
Caroline Mildred Gomes <carolinemildredgomes@gmail.com>
2026-04-08 21:16:02 +0600

    pizza.py

---
+++ File Input & Output /🍕 Pizza Py/pizza.py
@@ -0,0 +1,29 @@
+import sys
+import csv
+from tabulate import tabulate
+
+
+def main():
+    if len(sys.argv) < 2:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+
+    filename = sys.argv[1]
+
+    if not filename.endswith(".csv"):
+        sys.exit("Not a CSV file")
+
+    try:
+        with open(filename, "r", newline="") as file:
+            reader = csv.reader(file)
+            table = list(reader)
+
+            print(tabulate(table[1:], headers=table[0], tablefmt="grid"))
+
+    except FileNotFoundError:
+        sys.exit("File does not exist")
+
+
+if __name__ == "__main__":
+    main()
